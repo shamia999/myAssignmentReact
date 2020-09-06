b368ee12383a1ae483cd58b0a2fb3656d4f1cd2f
unknown <naila.shah777@gmail.com>
2020-09-06 19:00:04 +0530

    created skeleton of home page

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-class App extends Component{
-  render()
-  {
-  return (
-    <div>
-      phone Directory
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <div className="header">
+          phone Directory
     </div>
-  );
-}
+        <button>Add</button>
+        <div>
+          <span>Name</span><br/>
+          <span>Phone</span>
+
+        </div> */}
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" placeholder="type here" defaultValue="naila"/>
+      </div>
+    );
+  }
 }
 
 export default App;
