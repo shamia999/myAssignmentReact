22b18756d6cdf3869284199d31a7cfce4a2424a8
unknown <naila.shah777@gmail.com>
2020-09-12 13:30:28 +0530

    home page of phone-directory

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import Header from "./Header"
+import Header from "./Header";
+import "./App.css";
 class App extends Component {
   render() {
                                                                                 
     return (
       <div>
          <Header/>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br/>
-          <span>Phone</span>
+         <div className="bodyContainer">
+           <button className="btn1">Add</button>
+         <div className="content">
+          <span className="name">Name</span>
+          <span className="phone">Phone</span>
      </div> 
+     </div>
       </div>
     );
   }
